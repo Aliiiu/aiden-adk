c18524b0f201beb136d21c61237fdebf46dec886
Adebesin Tolulope <adebesintolulope80@gmail.com>
2025-11-10 09:26:00 +0100

    refactor: Update generic type in formatData function

--- src/lib/helpers/custom-json-formatter.ts
+++ src/lib/helpers/custom-json-formatter.ts
@@ -1,4 +1,4 @@
-export function formatData(headers: string[], rows: any[][]): string {
+export function formatData<T>(headers: string[], rows: T[][]): string {
 	const headerLine = headers.join("|");
 	const dataLines = rows.map((row) => row.join("|")).join("\n");
 	return `${headerLine}\n${dataLines}`;
